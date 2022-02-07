d0ab9f3e8584209562543dbafb35502c050ef482
CIEAB-Code <60346590+CIEAB-Code@users.noreply.github.com>
2022-02-07 19:23:46 +0000

    use a while loop to decrease complexity
    use a while loop to decrease complexity as the calculation continues as list is being reduced after each loop

--- findSubsets.py
+++ findSubsets.py
@@ -1,5 +1,4 @@
 from itertools import combinations
-import time
 
 
 def findSubsets(setList):
@@ -16,9 +15,29 @@ def findSubsets(setList):
     return last_changed
 
 
+def findSubsetsWhile(lstSets):
+    last_changed = lstSets.copy()
+    to_change = lstSets.copy()
+    index = 0
+    eleLeft = len(to_change)
+    while eleLeft > index:
+        rem = 0
+        for i in list(range(eleLeft)):
+            if i == len(last_changed)-(index+1):
+                break
+            elif len(last_changed[index].intersection(last_changed[index + i + 1])) >= 3:
+                to_change.remove(last_changed[index+i+1])
+                rem += 1
+        index += 1
+        eleLeft -= rem
+        last_changed = to_change.copy()
+    print("WHILE LENGTH", len(last_changed))
+    return last_changed
+
+
 if __name__ == "__main__":
     num = 16
     lst = combinations(range(1, num + 1), 4)
     lstSets = [set(i) for i in lst]
     print(f"LENGTH: {len(lstSets)}")
-    print(findSubsets(lstSets))
+    print(findSubsetsWhile(lstSets))
